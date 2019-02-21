31158d71fd5be2e7814c23fe56399ae207ca77c3
Oleksandr.Usatyi <belarafon3d@gmail.com>
2019-02-21 21:08:35 +0200

    comments add

--- _assets/main.js
+++ _assets/main.js
@@ -1,30 +1,27 @@
 ;(function ($) {
-    function isMobile() {    // preve
+    function isMobile() {    // check if user agent is mobile
         return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     }
 
-    if (isMobile()) {
-        return;
+    if (!isMobile()) {   // run only on non mobile browsers
+        var btnSelector = '.promo__btn',
+            blockSelector = '.promo__col',
+            imageSelector = '.promo__image',
+            focusClass = 'focused',
+            blurClass = 'blured';
+
+        $(document).on('mouseenter mouseleave', btnSelector, function (e) {
+            var btn = $(this),
+                scope = btn.closest(blockSelector),
+                img = scope.find(imageSelector),
+                siblingImg = scope.siblings(blockSelector).find(imageSelector);
+
+            if (e.type === 'mouseenter') {
+                img.addClass(focusClass);
+                siblingImg.addClass(blurClass);
+            } else {
+                $(imageSelector).removeClass(focusClass + ' ' + blurClass)     // clear image classes
+            }
+        });
     }
-
-
-    var btnSelector = '.promo__btn',
-        blockSelector = '.promo__col',
-        imageSelector = '.promo__image',
-        focusClass = 'focused',
-        blurClass = 'blured';
-
-    $(document).on('mouseenter mouseleave', btnSelector, function (e) {
-        var btn = $(this),
-            scope = btn.closest(blockSelector),
-            img = scope.find(imageSelector),
-            siblingImg = scope.siblings(blockSelector).find(imageSelector);
-
-        if (e.type === 'mouseenter') {
-            img.addClass(focusClass);
-            siblingImg.addClass(blurClass);
-        } else {
-            $(imageSelector).removeClass(focusClass + ' ' + blurClass)
-        }
-    });
 })(jQuery);
